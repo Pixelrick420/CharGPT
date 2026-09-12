26394e72d5f6c04199a86942156ca4add8a9d367
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-13 00:22:26 +0530

    Step 62: forward_logits_lookup

--- model.py
+++ model.py
@@ -392,8 +392,9 @@ def observe_lookup_equivalence(w, ids):
         'index_result': w[ids]
     }
 
-# Step 62 - forward_logits_lookup (not yet solved)
-# TODO: implement
+# Step 62 - forward_logits_lookup
+def forward_logits_lookup(w, ids):
+    return w[ids]
 
 # Step 63 - logits_to_probs_rowwise (not yet solved)
 # TODO: implement
