dae93a26537ef9cc2a9f76149ef28f1bc0dd1f9d
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-12 18:13:40 +0530

    Step 48: add_one_smoothing

--- model.py
+++ model.py
@@ -303,8 +303,11 @@ def vectorize_counts_add_at(vocab_size, data):
     np.add.at(count_matrix, (data[:-1], data[1:]), 1)
     return count_matrix
 
-# Step 48 - add_one_smoothing (not yet solved)
-# TODO: implement
+# Step 48 - add_one_smoothing
+import numpy as np
+
+def add_one_smoothing(n_matrix):
+    return n_matrix + 1
 
 # Step 49 - row_sums_of_counts (not yet solved)
 # TODO: implement
