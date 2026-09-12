aaa7336720d28ada81aca404a321d992a6fa67e9
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-12 16:32:23 +0530

    Step 34: read_text_file

--- model.py
+++ model.py
@@ -211,8 +211,14 @@ def stable_softmax_2d_rowwise(logits):
     sum_exp = sum_keepdims(exp_logits, axis=1)
     return exp_logits / sum_exp
 
-# Step 34 - read_text_file (not yet solved)
-# TODO: implement
+# Step 34 - read_text_file
+def read_text_file(text_blob):
+    if not isinstance(text_blob, str):
+        raise TypeError()
+    elif text_blob:
+        return text_blob
+    else:
+        raise ValueError()
 
 # Step 35 - encode_corpus_to_int_array (not yet solved)
 # TODO: implement
