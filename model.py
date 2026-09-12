5b617f4b7d6f4939f03b2e94582b2582a5fe85a4
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-12 15:27:37 +0530

    Step 26: max_along_axis

--- model.py
+++ model.py
@@ -154,8 +154,11 @@ import numpy as np
 def sum_axis1(arr):
     return np.sum(arr, axis=1)
 
-# Step 26 - max_along_axis (not yet solved)
-# TODO: implement
+# Step 26 - max_along_axis
+import numpy as np
+
+def max_along_axis(arr, axis):
+    return np.max(arr, axis=axis)
 
 # Step 27 - matmul (not yet solved)
 # TODO: implement
