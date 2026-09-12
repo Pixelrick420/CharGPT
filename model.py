7c3d64eaf96ecae9f6a9a796be117ccbb0eefd4f
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-13 00:13:22 +0530

    Step 60: forward_logits_onehot

--- model.py
+++ model.py
@@ -376,8 +376,9 @@ def one_hot_encode_batch(ids, vocab_size):
     onehot[rows, ids] = 1.0
     return onehot
 
-# Step 60 - forward_logits_onehot (not yet solved)
-# TODO: implement
+# Step 60 - forward_logits_onehot
+def forward_logits_onehot(onehot, w_matrix):
+    return matmul(onehot, w_matrix)
 
 # Step 61 - observe_lookup_equivalence (not yet solved)
 # TODO: implement
