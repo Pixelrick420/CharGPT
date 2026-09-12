e0c13223f41046285464ee926122ec6fd14ee9c3
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-12 22:42:22 +0530

    Step 58: scale_w_small

--- model.py
+++ model.py
@@ -360,8 +360,11 @@ import numpy as np
 def initialize_w_random(vocab_size, rng):
     return rng.standard_normal(size=(vocab_size, vocab_size))
 
-# Step 58 - scale_w_small (not yet solved)
-# TODO: implement
+# Step 58 - scale_w_small
+import numpy as np
+
+def scale_w_small(w_matrix, scale):
+    return w_matrix * scale
 
 # Step 59 - one_hot_encode_batch (not yet solved)
 # TODO: implement
