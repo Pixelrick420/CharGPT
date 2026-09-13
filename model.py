4732f63eab111e93c9d2009723520abc8b72d1e4
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-13 22:24:25 +0530

    Step 95: create_positional_embedding

--- model.py
+++ model.py
@@ -699,8 +699,10 @@ def token_embedding_backward(d_out, cache):
     
     return dE
 
-# Step 95 - create_positional_embedding (not yet solved)
-# TODO: implement
+# Step 95 - create_positional_embedding
+def create_positional_embedding(block_size, d_model, scale=0.02):
+    P = make_2d_random(block_size, d_model, seed=None)
+    return scale_w_small(P, scale)
 
 # Step 96 - slice_positional_embedding (not yet solved)
 # TODO: implement
