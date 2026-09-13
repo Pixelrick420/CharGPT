7496f4ff748b4b1248d4b2862f8fee622b6023c7
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-13 15:25:08 +0530

    Step 78: linear_backward_dw

--- model.py
+++ model.py
@@ -536,8 +536,10 @@ def linear_backward_dx(dy, cache):
     w = cache['w']
     return dy.dot(w.T)
 
-# Step 78 - linear_backward_dw (not yet solved)
-# TODO: implement
+# Step 78 - linear_backward_dw
+def linear_backward_dw(dy, cache):
+    x = cache['x']
+    return x.T.dot(dy)
 
 # Step 79 - bias_add_forward (not yet solved)
 # TODO: implement
