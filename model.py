7626ebd3aced2d27ee321e07006d9e9364fb98e3
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-14 23:29:25 +0530

    Step 100: compute_query

--- model.py
+++ model.py
@@ -729,8 +729,9 @@ def create_qkv_projections(d_model, d_head, scale=0.02):
         'Wv': scale_w_small(make_2d_random(d_model, d_head, 2), scale)
     }
 
-# Step 100 - compute_query (not yet solved)
-# TODO: implement
+# Step 100 - compute_query
+def compute_query(x, w_q):
+    return x @ w_q
 
 # Step 101 - compute_key (not yet solved)
 # TODO: implement
