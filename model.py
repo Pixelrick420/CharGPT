9bd63aeeebe8d729d6442af4df2c2c17e3553c95
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-14 23:49:30 +0530

    Step 113: scale_scores_backward

--- model.py
+++ model.py
@@ -816,8 +816,9 @@ def masked_softmax_backward(d_attn, cache):
     
     return d_scores
 
-# Step 113 - scale_scores_backward (not yet solved)
-# TODO: implement
+# Step 113 - scale_scores_backward
+def scale_scores_backward(d_scaled_scores, d_head):
+    return d_scaled_scores / np.sqrt(d_head)
 
 # Step 114 - qk_scores_backward (not yet solved)
 # TODO: implement
