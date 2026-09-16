23dec324f57f4f598fce8c9c15e6d763ca376c39
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-16 15:48:34 +0530

    Step 132: ffn_activation_forward

--- model.py
+++ model.py
@@ -968,8 +968,11 @@ def ffn_linear_one_forward(x, w1, b1):
         }
     }
 
-# Step 132 - ffn_activation_forward (not yet solved)
-# TODO: implement
+# Step 132 - ffn_activation_forward
+def ffn_activation_forward(h1):
+    relu_res = relu_forward(h1)
+    
+    return relu_res['y'], {'h1': h1}
 
 # Step 133 - ffn_linear_two_forward (not yet solved)
 # TODO: implement
