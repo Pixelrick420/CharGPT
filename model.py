d57bb0599d68711fd9d02cd0ce24677a4d3e4d3a
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2026-09-20 01:56:45 +0530

    Step 148: initialize_adam_step_counter

--- model.py
+++ model.py
@@ -1382,8 +1382,9 @@ def initialize_adam_moments(model_params):
     
     return m, v
 
-# Step 148 - initialize_adam_step_counter (not yet solved)
-# TODO: implement
+# Step 148 - initialize_adam_step_counter
+def initialize_adam_step_counter():
+    return 0
 
 # Step 149 - adam_increment_step (not yet solved)
 # TODO: implement
